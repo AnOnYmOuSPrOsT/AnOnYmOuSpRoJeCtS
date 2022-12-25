581c1472e0c0e33eab504f13679beb3c8f6c6152
PrOsT <PrOsT@DESKTOP-5C0RHB8>
2022-12-25 20:47:39 +0300

    Добавьте файлы проекта.

---
+++ AnOnYmOuSpRoJeCtS.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "AnOnYmOuSpRoJeCtS.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            wprintf(L"Критическая ошибка: сбой при инициализации MFC\n");
+            nRetCode = 1;
+        }
+        else
+        {
+
+        }
+    }
+    else
+    {
+        wprintf(L"Критическая ошибка: сбой GetModuleHandle\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
